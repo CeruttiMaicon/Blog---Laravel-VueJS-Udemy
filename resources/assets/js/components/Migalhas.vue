b6bb46ff1a74445f8457282ee161f65c092ecbb5
regexp <=>
2018-07-19 18:36:21 -0300

    Aula 23 - Seção 1 - Curso Laravel 5.5 com Vue JS

---
+++ resources/assets/js/components/Migalhas.vue
@@ -0,0 +1,18 @@
+<template>
+<nav aria-label="breadcrumb">
+    <ol class="breadcrumb">
+        <li class="breadcrumb-item active"><a href="#">Home</a> </li>
+        <li class="breadcrumb-item active"><a href="#">Library</a> </li>
+        <li class="breadcrumb-item active"> Data</li>
+    </ol>
+</nav>
+</template>
+
+<script>
+    export default {
+        props:['lista'],
+        mounted:function() {
+            console.log(this.lista);
+        },
+    }
+</script>
--- resources/views/admin/artigos/index.blade.php
+++ resources/views/admin/artigos/index.blade.php
@@ -4,7 +4,7 @@
 
 <pagina tamanho="10">
     <painel titulo="Lista de Artigos">
-
+    <migalhas v-bind:lista="{{$listaMigalhas}}"></migalhas>
         <tabela-lista 
         v-bind:titulos="['#', 'Titulo', 'Descricao']" 
         v-bind:itens="[[1, 'PHP OO', 'Curso de PHP'], [2, 'Vue JS', 'Curso de Vue JS']]"
